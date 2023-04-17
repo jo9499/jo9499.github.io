58c95d18725f00d3c27554675bfdd98a4820907c
jo <126530579+jo9499@users.noreply.github.com>
2023-04-18 00:12:54 +1000

    Add files via upload

---
+++ im-2310/a2/e2/arrays-&-for-loops.js
@@ -0,0 +1,25 @@
+var fruit1 = "banana";
+var fruit2 = "pineapple";
+var fruit3 = "apple";
+var fruit4 = "peach";
+var fruit5 = "orange";
+
+var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
+
+var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
+
+console.log(fruitArray)
+
+fruitArray.push("tomato")
+
+console.log(fruitArray);
+
+
+for(let fruit of fruitArray){
+    //console.log("hi");
+    if(fruit === "banana"){
+        console.log("i'm a banana")
+    } else {
+        console.log(fruit)
+    }
+}
